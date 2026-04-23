2abf650343ced9a3718b4523eeb04053311dc84a
Matthew Szatmary <matt@szatmary.org>
2026-04-23 10:44:33 -0700

    Graphs: add ScienceMetricProvider

---
+++ Graphs/Metrics/Providers/ScienceMetricProvider.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using Timberborn.ScienceSystem;
+
+namespace Graphs.Metrics.Providers;
+
+public sealed class ScienceMetricProvider : IMetricProvider
+{
+    private readonly ScienceService _scienceService;
+
+    public ScienceMetricProvider(ScienceService scienceService)
+    {
+        _scienceService = scienceService;
+    }
+
+    public IEnumerable<MetricDefinition> GetMetrics()
+    {
+        yield return new MetricDefinition(
+            id: "science.stored",
+            nameLocKey: "Graphs.Metric.ScienceStored",
+            category: MetricCategory.Science,
+            scope: MetricScope.Settlement,
+            valueFn: _ => _scienceService.SciencePoints);
+    }
+}
